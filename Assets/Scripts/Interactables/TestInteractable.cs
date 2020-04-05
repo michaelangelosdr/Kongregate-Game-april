d6ee7b27c1591c8207e01fe6df7661a8af712e8f
Michael angelo Del rosario <michaelangelosdr@gmail.com>
2020-04-05 15:43:34 +0800

    merge gamplay enemy branch

--- Assets/Scripts/Interactables/TestInteractable.cs
+++ Assets/Scripts/Interactables/TestInteractable.cs
@@ -4,9 +4,27 @@ using UnityEngine;
 
 public class TestInteractable : MonoBehaviour, IInteractable
 {
+
+    [SerializeField]
+    SpriteRenderer renderer;
+
+    private bool isInteractedWith = false;
+
+
     public void Interact()
     {
+        isInteractedWith = !isInteractedWith;
         Debug.Log("HELLO YOU HAVE INTERACTED WITH ME");
+        
+        if(isInteractedWith)
+        {
+            renderer.color = Color.blue;
+        }
+        else
+
+        {
+            renderer.color = Color.white;
+        }
     }
 
     public IEnumerator _Interact()
