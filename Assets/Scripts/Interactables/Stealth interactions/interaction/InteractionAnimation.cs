4eef5c972c10a449e4960102f7f929c26075f628
Michael angelo Del rosario <michaelangelosdr@gmail.com>
2020-04-15 20:27:01 +0800

    Added stealth bar mechanic

---
+++ Assets/Scripts/Interactables/Stealth interactions/interaction/InteractionAnimation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionAnimation : MonoBehaviour
+{
+    [SerializeField] private Animation m_Animation;
+    [SerializeField] public string InteractionInfo;
+
+
+    public virtual void PlayInteractionAnimation()
+    {
+        m_Animation.Play();
+    }
+
+
+
+}
---
+++ Assets/Scripts/Modules/StealthModule.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StealthModule : GameModule
+{
+    [SerializeField] private int Max_StealthValue;
+    [SerializeField] private float StealthValueMultiplier;
+    [SerializeField] public int StealthValue;
+    [SerializeField] private StealthBarUI stealthBarUI;
+
+
+
+
+    public override void onAwake()
+    {
+        if (stealthBarUI == null)
+        {
+            stealthBarUI = GameObject.Find("Stealth Bar Canvas").GetComponent<StealthBarUI>();
+        }
+
+        stealthBarUI.SetStealthBarVal(StealthValue, "Game start");
+    }
+
+    public override void onStart()
+    {       
+       stealthBarUI.SetStealthBarVal(StealthValue,"Game start");
+    }
+
+    public override void Refresh()
+    {
+        //Debug.Log(StealthValue);
+        //stealthBarUI.SetStealthBarVal(StealthValue ," ");  
+    }
+
+    public void Found()
+    {
+        StealthValue = StealthValue - (int)(Time.deltaTime * StealthValueMultiplier);
+        stealthBarUI.SetStealthBarVal(StealthValue);
+    }
+    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Stealth Damager Object"))
+        {
+            StealthDamager d = collision.GetComponent<StealthDamager>();
+
+            StealthValue -=d.GetDamageValue();
+            stealthBarUI.SetStealthBarVal(StealthValue, d.GetDamageInfo());
+        }
+    }
+
+}
