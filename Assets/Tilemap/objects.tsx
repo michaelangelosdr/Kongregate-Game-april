dc896544273be5a11fc9decf540aa4b2069402a1
Juelle Q <juelleq@gmail.com>
2020-04-16 20:16:25 +0800

    Uploaded Tilemap and objects using SuperTiled2Unity

---
+++ Assets/Tilemap/objects.tsx
@@ -0,0 +1,86 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="objects" tilewidth="216" tileheight="134" tilecount="17" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="43" height="40" source="../Object/Button.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.549199" y="1.0984" width="41.7391" height="36.2471"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <image width="19" height="29" source="../Object/Button2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="50" height="56" source="../Object/Crate.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3.29519" y="1.0984" width="45.5835" height="52.1739"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <image width="50" height="56" source="../Object/Crate2.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1.6476" y="3.29519" width="46.6819" height="49.4279"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <image width="16" height="65" source="../Object/Door.png"/>
+ </tile>
+ <tile id="5">
+  <image width="59" height="69" source="../Object/Enemy.png"/>
+ </tile>
+ <tile id="6">
+  <image width="63" height="40" source="../Object/Floor Panel.png"/>
+ </tile>
+ <tile id="7">
+  <image width="48" height="54" source="../Object/OilCan.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-2.1968" y="-0.549199" width="48.8787" height="53.8215"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <image width="58" height="49" source="../Object/pipe.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2.1968" y="-1.0984" width="54.9199" height="25.8124"/>
+   <object id="2" x="0" y="21.4188" width="23.6156" height="25.8124"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <image width="216" height="95" source="../Object/Pipe2.png"/>
+ </tile>
+ <tile id="10">
+  <image width="54" height="64" source="../Object/Sack.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1.0984" y="-0.549199" width="52.7231" height="64.8055"/>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <image width="59" height="112" source="../Object/SafeZone.png"/>
+ </tile>
+ <tile id="12">
+  <image width="89" height="44" source="../Object/Shelf H 2.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1.0984" y="0" width="86.7735" height="41.7391"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <image width="57" height="134" source="../Object/Shelf.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2.746" y="2.746" width="47.2311" height="129.611"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <image width="74" height="57" source="../Object/Table.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.49199" y="10.4348" width="64.8055" height="45.0343"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <image width="152" height="62" source="../Object/Shelf2.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4.94279" y="3.29519" width="146.636" height="58.2151"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <image width="50" height="61" source="../Object/Guy.png"/>
+ </tile>
+</tileset>
