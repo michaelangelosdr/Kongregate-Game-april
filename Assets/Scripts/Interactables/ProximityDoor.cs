0aa38af113cabd0a6a30f5b6995878b276b1f45a
Michael angelo Del rosario <michaelangelosdr@gmail.com>
2020-04-16 13:44:40 +0800

    Added proximity door with enemy check

---
+++ Assets/Scripts/Interactables/ProximityDoor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProximityDoor : Door
+{
+    [SerializeField]
+    private SpriteRenderer leftDoor;
+
+    [SerializeField]
+    private SpriteRenderer rightDoor;
+
+    public override void Interact()
+    {
+        
+    }
+
+    public void inProximity(bool isWithin)
+    {
+        isOpened = isWithin;
+        OpenDoor();
+    }
+
+    public override void OpenDoor()
+    {
+        if(isOpened)
+        {
+            LeanTween.moveLocalY(leftDoor.gameObject, 0.9f, 0.5f);
+            LeanTween.moveLocalY(rightDoor.gameObject, -0.9f, 0.5f);
+        }
+        else
+        {
+
+            LeanTween.moveLocalY(leftDoor.gameObject, 0.4f, 0.5f);
+            LeanTween.moveLocalY(rightDoor.gameObject, -0.4f, 0.5f);
+        }
+    }
+
+}
