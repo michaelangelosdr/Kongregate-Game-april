7666a34faf81b2f1a0771e9b1011d116895fcdcb
Michael angelo Del rosario <michaelangelosdr@gmail.com>
2020-04-23 21:38:32 +0800

    Fixed sprite rotation

--- Assets/Scripts/Modules/SpriteModule.cs
+++ Assets/Scripts/Modules/SpriteModule.cs
@@ -23,13 +23,13 @@ public class SpriteModule : GameModule
     public override void Refresh()
     {
 
-        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W)) { _renderer.transform.rotation = Quaternion.Euler(0, 0, 135); }
-        else if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.S)) { _renderer.transform.rotation = Quaternion.Euler(0, 0, 225); }
-        else if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.W)) { _renderer.transform.rotation = Quaternion.Euler(0, 0, 45); }
-        else if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.S)) { _renderer.transform.rotation = Quaternion.Euler(0, 0, 315); }
-        else if (Input.GetKey(KeyCode.A)) { _renderer.transform.rotation = Quaternion.Euler(0, 0, 180); }
-        else if (Input.GetKey(KeyCode.D)) { _renderer.transform.rotation = Quaternion.Euler(0, 0, 0); }
-        else if (Input.GetKey(KeyCode.W)) { _renderer.transform.rotation = Quaternion.Euler(0, 0, 90); }
-        else if (Input.GetKey(KeyCode.S)) { _renderer.transform.rotation = Quaternion.Euler(0, 0, 270); }
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W)) { _renderer.transform.rotation = Quaternion.Euler(0, 0, 225); }
+        else if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.S)) { _renderer.transform.rotation = Quaternion.Euler(0, 0, 315); }//225
+        else if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.W)) { _renderer.transform.rotation = Quaternion.Euler(0, 0, 135); }
+        else if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.S)) { _renderer.transform.rotation = Quaternion.Euler(0, 0, 45); }//315
+        else if (Input.GetKey(KeyCode.A)) { _renderer.transform.rotation = Quaternion.Euler(0, 0, 270); }
+        else if (Input.GetKey(KeyCode.D)) { _renderer.transform.rotation = Quaternion.Euler(0, 0, 90); }
+        else if (Input.GetKey(KeyCode.W)) { _renderer.transform.rotation = Quaternion.Euler(0, 0, 180); }
+        else if (Input.GetKey(KeyCode.S)) { _renderer.transform.rotation = Quaternion.Euler(0, 0, 0); }
     }    
 }
