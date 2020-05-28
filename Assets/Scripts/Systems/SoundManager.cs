f66c37ba6ea5e41d45a933052392585a30d14bab
Michael angelo Del rosario <michaelangelosdr@gmail.com>
2020-05-28 13:12:11 +0800

    commit message 1

---
+++ Assets/Scripts/Systems/SoundManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundManager : MonoBehaviour
+{
+
+    [SerializeField]
+    AudioSource m_AudioSource; 
+
+
+    public void PlaySound()
+    {
+
+    }
+
+    public void LoopSound()
+    {
+
+    }
+
+}
